d61b0f34ed5fd463f690a9478b51fa431ff19eb8
raylu <90059+raylu@users.noreply.github.com>
2022-10-20 21:24:51 -0700

    boilerplate

---
+++ README.md
@@ -0,0 +1,4 @@
+```sh
+$ gdre_tools/gdre_tools.x86_64 --headless --output-dir=extracted \
+	--recover='/mnt/.../Steam/steamapps/common/Spellbook Demonslayers Prologue/Spellbook Demonslayers.pck'
+```
---
+++ sbds_tools.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+
+import sys
+if len(sys.argv) == 3:
+	import eventlet
+	import eventlet.wsgi
+	eventlet.monkey_patch()
+
+# pylint: disable=wrong-import-position
+import mimetypes
+
+from pigwig import PigWig, Response
+from pigwig.exceptions import HTTPException
+
+def root(request):
+	return Response.render(request, 'index.jinja2', {'no_back_button': True})
+
+def spells_page(request):
+	return Response.render(request, 'spells.jinja2', {})
+
+def static(request, path):
+	content_type, _ = mimetypes.guess_type(path)
+	try:
+		with open('static/' + path, 'rb') as f:
+			return Response(f.read(), content_type=content_type)
+	except FileNotFoundError:
+		raise HTTPException(404, '%r not found\n' % path) # pylint: disable=raise-missing-from
+
+routes = [
+	('GET', '/', root),
+	('GET', '/spells', spells_page),
+	('GET', '/static/<path:path>', static),
+]
+
+app = PigWig(routes, template_dir='templates')
+
+def main():
+	if len(sys.argv) == 3:
+		addr = sys.argv[1]
+		port = int(sys.argv[2])
+		eventlet.wsgi.server(eventlet.listen((addr, port)), app)
+	else:
+		app.main()
+
+if __name__ == '__main__':
+	main()
