18d54c45e66186b96e07a691cd61c80afd57c5b9
raylu <90059+raylu@users.noreply.github.com>
2022-10-22 15:08:52 -0700

    language switcher

--- static/spells.js
+++ static/spells.js
@@ -8,30 +8,51 @@
 	const spellsPromise = fetchJSON('/static/data/spells.json');
 	const translationPromise = fetchJSON('/static/data/translations.json');
 	const spells = await spellsPromise;
-	const {translations} = await translationPromise;
+	const {languages, translations} = await translationPromise;
 
+	const langs = document.querySelector('#langs');
 	let lang = 'en';
+	for (const langCode of languages) {
+		const div = document.createElement('div');
+		div.innerText = div.dataset['lang'] = langCode;
+		if (langCode === lang)
+			div.classList.add('selected');
+		langs.appendChild(div);
+	}
+	langs.addEventListener('click', (event) => {
+		const newLang = event.target.dataset['lang'];
+		if (newLang) {
+			langs.querySelectorAll('div').forEach((div) => div.classList.remove('selected'));
+			event.target.classList.add('selected');
+			lang = newLang;
+			render();
+		}
+	});
 
 	const main = document.querySelector('main');
-	for (const [spellID, data] of Object.entries(spells['SPELL'])) {
-		main.appendChild(renderSpell(spellID, data));
+	function render() {
+		main.innerHTML = '';
+		for (const [spellID, data] of Object.entries(spells['SPELL'])) {
+			main.appendChild(renderSpell(spellID, data));
 
-		for (const evolveName of data['evolveList'] || []) {
-			let evolveData;
-			if (evolveName == 'SPELL_INDRA_SIGIL')
-				evolveData = spells['SPELL'][evolveName];
-			else
-				evolveData = spells['EVOLVED'][evolveName];
-			const section = renderSpell(evolveName, evolveData);
-			section.classList.add('evolved');
-			main.appendChild(section);
+			for (const evolveName of data['evolveList'] || []) {
+				let evolveData;
+				if (evolveName == 'SPELL_INDRA_SIGIL')
+					evolveData = spells['SPELL'][evolveName];
+				else
+					evolveData = spells['EVOLVED'][evolveName];
+				const section = renderSpell(evolveName, evolveData);
+				section.classList.add('evolved');
+				main.appendChild(section);
+			}
 		}
 	}
 
 	function renderSpell(spellID, data) {
 		const section = document.createElement('section');
 		const name = translate(data['spellName']);
-		section.innerHTML = `<img src="/static/data/spells/${spellID}.png"><h3>${name}:</h3>`;
+		section.innerHTML = `<img loading="lazy" src="/static/data/spells/${spellID}.png">` +
+				`<h3>${name}:</h3>`;
 		for (const [key, value] of Object.entries(data)) {
 			if (Array.isArray(value))
 				section.innerHTML += `<div>${key}: <pre>${value.join('\n')}</pre></div>`;
@@ -48,4 +69,6 @@
 		else // translation not found
 			return s;
 	}
+
+	render();
 })();
