eca4170de3f2e1d094f9d484b45a4a9877ba2dc4
raylu <90059+raylu@users.noreply.github.com>
2022-10-22 14:40:55 -0700

    use translations

--- extract.py
+++ extract.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import csv
 import json
 import os
 import re
@@ -12,10 +13,28 @@ def main():
 	for d in ['static/data', 'static/data/spells']:
 		os.mkdir(d)
 
+	langs, translations = prepare_translations()
+	with open('static/data/translations.json', 'w') as f:
+		json.dump({'languages': list(langs), 'translations': translations}, f, indent='\t')
 	with open('static/data/spells.json', 'w') as f:
-		json.dump(load_spells(), f, indent='\t')
+		json.dump(prepare_spells(), f, indent='\t')
 
-def load_spells() -> dict:
+def prepare_translations() -> tuple[set, dict[str, dict[str, str]]]:
+	translations = {}
+	langs = None
+	with open('extracted/SBDS Translations - General Terms.csv', 'r', encoding='utf-8') as f:
+		reader = csv.DictReader(f)
+		row = next(reader)
+		langs = frozenset(lang for lang, s in row.items() if s and lang != 'keys')
+		print('languages:', ', '.join(langs))
+		translations[row['keys']] = {lang: s for lang, s in row.items() if lang in langs}
+		for row in reader:
+			if len(row['keys']) == 0:
+				continue
+			translations[row['keys']] = {lang: s for lang, s in row.items() if lang in langs}
+	return langs, translations
+
+def prepare_spells() -> dict:
 	spell_paths = {'SPELL': [], 'EVOLVED': [], 'AURA': []}
 	path_re = re.compile(r'preload\("res://(.+)"\)')
 	with open('extracted/SpellReferenceList.gd', 'r', encoding='ascii') as f:
@@ -26,33 +45,36 @@ def load_spells() -> dict:
 			prefix, name, paths = m.groups()
 			if prefix in ('SPELL', 'EVOLVED'):
 				path = path_re.match(paths).group(1)
-				spell_paths[prefix].append((name, path))
+				spell_paths[prefix].append(path)
 			else:
 				assert prefix == 'AURA'
 				paths = re.match(r'\[(.+), (.+), "\w+"\]', paths).groups()
 				paths = [path_re.match(path).group(1) for path in paths]
-				spell_paths[prefix].append((name, paths))
+				spell_paths[prefix].append(paths)
 
 	spells = {
 		'SPELL': {},
 		'EVOLVED': {},
 	}
 	fields = [
+		'spellName',
 		'baseDamage',
 		'baseCooldown',
 		'projectileAmount',
+		'multiProjectileDelay',
 		'levelUpDescriptions',
 		'evolveList',
 		'spellTags',
 	]
 	for prefix in ('SPELL', 'EVOLVED'):
-		for name, path in spell_paths[prefix]:
+		for path in spell_paths[prefix]:
 			scene = godot_parser.load('extracted/' + path)
 
 			node = scene.find_node(parent=None)
+			spell_id = node['spellID']
 			spell = {key: node.get(key) for key in fields}
 			assert prefix == 'SPELL' or spell['evolveList'] is None
-			spells[prefix][name] = spell
+			spells[prefix][spell_id] = spell
 
 			icon = node.get('newLevelUpIcon', node.get('levelUpIcon'))
 			resource = scene.find_ext_resource(id=icon.id)
@@ -61,8 +83,8 @@ def load_spells() -> dict:
 				resource = scene.find_ext_resource(type='Texture')
 			assert resource.path.startswith('res://')
 			img_path = resource.path[len('res://'):]
-			print(name, '→', img_path)
-			os.link('extracted/' + img_path, f'static/data/spells/{name}.png')
+			print(spell_id, '→', img_path)
+			os.link('extracted/' + img_path, f'static/data/spells/{spell_id}.png')
 	
 	return spells
 
--- static/spells.js
+++ static/spells.js
@@ -1,31 +1,37 @@
 'use strict';
 
 (async () => {
-	const res = await fetch('/static/data/spells.json');
-	const spells = await res.json();
+	async function fetchJSON(path) {
+		const res = await fetch(path);
+		return await res.json();
+	}
+	const spellsPromise = fetchJSON('/static/data/spells.json');
+	const translationPromise = fetchJSON('/static/data/translations.json');
+	const spells = await spellsPromise;
+	const {translations} = await translationPromise;
+
+	let lang = 'en';
 
 	const main = document.querySelector('main');
-	for (const [name, data] of Object.entries(spells['SPELL'])) {
-		main.appendChild(renderSpell(name, data));
+	for (const [spellID, data] of Object.entries(spells['SPELL'])) {
+		main.appendChild(renderSpell(spellID, data));
 
-		for (let evolveName of data['evolveList'] || []) {
+		for (const evolveName of data['evolveList'] || []) {
 			let evolveData;
-			if (evolveName == 'SPELL_INDRA_SIGIL') {
-				evolveName = 'INDRA_SIGIL';
+			if (evolveName == 'SPELL_INDRA_SIGIL')
 				evolveData = spells['SPELL'][evolveName];
-			} else {
-				evolveName = evolveName.substr('EVOLVED_'.length);
+			else
 				evolveData = spells['EVOLVED'][evolveName];
-			}
 			const section = renderSpell(evolveName, evolveData);
 			section.classList.add('evolved');
 			main.appendChild(section);
 		}
 	}
 
-	function renderSpell(name, data) {
+	function renderSpell(spellID, data) {
 		const section = document.createElement('section');
-		section.innerHTML = `<img src="/static/data/spells/${name}.png"><h3>${name}:</h3>`;
+		const name = translate(data['spellName']);
+		section.innerHTML = `<img src="/static/data/spells/${spellID}.png"><h3>${name}:</h3>`;
 		for (const [key, value] of Object.entries(data)) {
 			if (Array.isArray(value))
 				section.innerHTML += `<div>${key}: <pre>${value.join('\n')}</pre></div>`;
@@ -33,5 +39,13 @@
 				section.innerHTML += `<div>${key}: ${value}</div>`;
 		}
 		return section;
+	}
+
+	function translate(s) {
+		const tr = translations[s];
+		if (tr)
+			return tr[lang];
+		else // translation not found
+			return s;
 	}
 })();
